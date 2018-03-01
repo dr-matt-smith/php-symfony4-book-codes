1e015638242fcb7cf3032bb628cd56e68105288a
dr-matt-smith <dr_matt_smith@me.com>
2018-03-01 12:29:14 +0000

    started security

---
+++ part-6-security/security01/src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
