4b2c9567f9d278727d02e53c6d4de9ed55916781
dr-matt-smith <dr_matt_smith@me.com>
2018-03-03 13:33:13 +0000

    login security completed

---
+++ part-6-security/security03_custom_form/src/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        $template = 'security/login.html.twig';
+        $args = [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ];
+
+        return $this->render($template, $args);
+    }
+}
