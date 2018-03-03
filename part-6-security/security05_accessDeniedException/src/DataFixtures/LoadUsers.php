471a59cd6fe6f0a0e4cff9e353420b42811eaf9c
dr-matt-smith <dr_matt_smith@me.com>
2018-03-03 18:57:07 +0000

    security completed

---
+++ part-6-security/security05_accessDeniedException/src/DataFixtures/LoadUsers.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class LoadUsers extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // create objects
+        $userUser = $this->createActiveUser('user', 'user@yser.com', 'user');
+        $userAdmin = $this->createActiveUser('admin', 'admin@admin.com', 'admin', ['ROLE_ADMIN']);
+        $userMatt = $this->createActiveUser('matt', 'matt@matt.com', 'smith', ['ROLE_ADMIN']);
+
+        // store to DB
+        $manager->persist($userUser);
+        $manager->persist($userAdmin);
+        $manager->persist($userMatt);
+        $manager->flush();
+    }
+
+    /**
+     * @param       $username
+     * @param       $email
+     * @param       $plainPassword
+     * @param array $roles // default to ROLE_USER if no ROLE supplied
+     *
+     * @return User
+     */
+    private function createActiveUser($username, $email, $plainPassword, $roles = ['ROLE_USER']):User
+    {
+        $user = new User();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setRoles($roles);
+        $user->setIsActive(true);
+
+        // password - and encoding
+        $encodedPassword = $this->encodePassword($user, $plainPassword);
+        $user->setPassword($encodedPassword);
+
+        return $user;
+    }
+
+    private function encodePassword($user, $plainPassword):string
+    {
+        $encodedPassword = $this->encoder->encodePassword($user, $plainPassword);
+        return $encodedPassword;
+    }
+}
