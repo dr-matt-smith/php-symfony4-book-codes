3bd4495a8972b40cede64cb6763a816e875107c0
dr-matt-smith <dr_matt_smith@me.com>
2018-03-06 08:01:56 +0000

    exception testing

---
+++ part-testing/test06_lowercase/src/Util/Calculator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Util;
+
+
+class Calculator
+{
+    public function add($n1, $n2)
+    {
+        return $n1 + $n2;
+    }
+
+    public function subtract($n1, $n2)
+    {
+        return $n1 - $n2;
+    }
+}
